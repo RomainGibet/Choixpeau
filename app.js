8a3b1374c1ff37c64dd33b5765a0c4f67cf444cd
RomainGibet <romain.gibet@gmail.com>
2022-12-15 15:37:57 +0100

    terminé

--- app.js
+++ app.js
@@ -13,56 +13,116 @@ let app = {
 
     init: function () {
 
-        
+
         let btnValidation = document.querySelector(".submit");
 
         btnValidation.addEventListener("click", app.handleSubmit);
-        
+
     },
 
     handleSubmit: function (e) {
 
         e.preventDefault();
-        
+
         let champNom = document.querySelector('input').value;
-        
+
 
         let reponseDuChoixpo = document.querySelector(".speech");
         reponseDuChoixpo.textContent = "";
 
+        let slotImg = document.createElement('img');
 
-        if (champNom == "") {
+        if (champNom.length === 0) {
+            
+            return reponseDuChoixpo.textContent = "Ça ne fonctionne qu'avec un nom, pardi !";
 
-            reponseDuChoixpo.textContent = "Ça ne fonctionne qu'avec un nom, pardi !";
+        } else if (champNom.length === 8) {
+
+
+            reponseDuChoixpo.prepend(slotImg);
+            slotImg.src = "./images/maxopus.png";
+
+        } else if (champNom.length % 5 === 0 || champNom.length % 3 === 0) {
+
+            reponseDuChoixpo.prepend(slotImg);
+            return slotImg.src = "./images/anthorvus.png";
+        
+        } else if (champNom.charAt(0) === "l" || champNom.charAt(champNom.length - 1) === "x") {
+
+            reponseDuChoixpo.prepend(slotImg);
+            return slotImg.src = "./images/lustrix.png"
+
 
         } else {
 
-
-
-            let randomNumber = Math.floor(Math.random() * ((4 - 0) + 0));
-
-
-            let slotImg = document.createElement('img');
-
-            for (let nomImage = 0; nomImage < app.houses.length; nomImage++) {
-
-
-            }
-
-
-            slotImg.src = "./images/" + app.houses[randomNumber] + ".png";
-
             reponseDuChoixpo.prepend(slotImg);
+            return slotImg.src = "./images/darioptera.png"
 
-        }
+
+
+        };
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     },
 
+};
 
 
 
-}
 
+
+
+// handleSubmit: function (e) {
+
+//     e.preventDefault();
+
+//     let champNom = document.querySelector('input').value;
+
+
+//     let reponseDuChoixpo = document.querySelector(".speech");
+//     reponseDuChoixpo.textContent = "";
+
+
+//     if (champNom == "") {
+
+//         reponseDuChoixpo.textContent = "Ça ne fonctionne qu'avec un nom, pardi !";
+
+//     } else {
+
+
+
+//         let randomNumber = Math.floor(Math.random() * ((4 - 0) + 0));
+
+
+//         let slotImg = document.createElement('img');
+
+//         for (let nomImage = 0; nomImage < app.houses.length; nomImage++) {
+
+
+//         }
+
+
+//         slotImg.src = "./images/" + app.houses[randomNumber] + ".png";
+
+//         reponseDuChoixpo.prepend(slotImg);
+
+//     }
+
+// },
 
 
 
